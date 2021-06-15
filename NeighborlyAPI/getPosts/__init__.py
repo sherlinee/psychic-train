453a3c88cbf13dec7bd82a2705aeb8345d456886
Sherline Kinyanjui <sherlinekinyanjui@gmail.com>
2021-06-15 21:55:13 +0300

    update

---
+++ NeighborlyAPI/getPosts/__init__.py
@@ -0,0 +1,23 @@
+import logging
+import azure.functions as func
+import pymongo
+import json
+from bson.json_util import dumps
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    logging.info('Python getPosts trigger function processed a request.')
+
+    try:
+        url = "mongodb://neighborlycosmosdb:lHI3xrP8VYditliiP2mxFAeIdSgWb9PHhIOBAFgB2x4Y5o7gEDlSikjnN9QWlBY14zQasLPDUKZMsZsbf0M95A==@neighborlycosmosdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@neighborlycosmosdb@"  # TODO: Update with appropriate MongoDB connection information
+        client = pymongo.MongoClient(url)
+        database = client['azure']
+        collection = database['posts']
+
+        result = collection.find({})
+        result = dumps(result)
+
+        return func.HttpResponse(result, mimetype="application/json", charset='utf-8', status_code=200)
+    except:
+        return func.HttpResponse("Bad request.", status_code=400)
---
+++ NeighborlyAPI/updateAdvertisement/__init__.py
@@ -0,0 +1,26 @@
+import azure.functions as func
+import pymongo
+from bson.objectid import ObjectId
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+
+    id = req.params.get('id')
+    request = req.get_json()
+
+    if request:
+        try:
+            url = "mongodb://neighborlycosmosdb:lHI3xrP8VYditliiP2mxFAeIdSgWb9PHhIOBAFgB2x4Y5o7gEDlSikjnN9QWlBY14zQasLPDUKZMsZsbf0M95A==@neighborlycosmosdb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@neighborlycosmosdb@"  # TODO: Update with appropriate MongoDB connection information
+            client = pymongo.MongoClient(url)
+            database = client['azure']
+            collection = database['advertisements']
+            
+            filter_query = {'_id': ObjectId(id)}
+            update_query = {"$set": eval(request)}
+            rec_id1 = collection.update_one(filter_query, update_query)
+            return func.HttpResponse(status_code=200)
+        except:
+            print("could not connect to mongodb")
+            return func.HttpResponse('Could not connect to mongodb', status_code=500)
+    else:
+        return func.HttpResponse('Please pass name in the body', status_code=400)
+
